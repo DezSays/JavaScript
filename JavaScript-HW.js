511926d1eaac06f90693b4e1033bcc0324d90120
Dez <dezareabryan@gmail.com>
2022-02-28 19:41:08 -0500

    Finally finished the first set of JavaScript homework problems

---
+++ JavaScript-HW.js
@@ -0,0 +1,109 @@
+// Hello You Part 1
+
+// function hello(name) {
+    
+// console.log('Hello, ' + name + "!");
+// }
+
+// hello("Mustache");
+
+
+
+
+// Hello You Part 2
+
+// function hello(name) {
+//     if(name.length > 0)
+//         console.log('Hello, ' + name + "!");
+//     else
+//         console.log('Hello, World! ');
+// }
+
+// hello("");
+
+
+
+
+
+// Madlib
+
+// function madlib(name, subject) {
+//     console.log(name + "'s favorite subject in school is " + subject)
+// }
+
+// madlib("Anushka", "art");
+
+
+
+
+// Tip Calculator 1
+
+// function tipAmount(bill, service){
+//     if(service == 'good'){
+//         tip = bill * .2
+//         console.log(tip);
+//     }
+//     else if(service == 'fair'){
+//         tip = bill * .15
+//         console.log(tip);
+//     }
+//     else if(service == 'bad'){
+//         tip = bill * .10
+//         console.log(tip);
+//     }
+// }
+
+// tipAmount(100, 'fair')
+
+
+
+
+// Tip Calculator 2
+
+// function totalAmount(bill, service){
+//     if(service == 'good'){
+//         tip = bill * .2
+//         billAmount = bill + tip
+//         console.log('Thank you for your business! Your total is $' + billAmount + '.');
+//     }
+//     else if(service == 'fair'){
+//         tip = bill * .15
+//         billAmount = bill + tip
+//         console.log('Thank you for your business! Your total is $' + billAmount + '.');
+//     }
+//     else if(service == 'bad'){
+//     tip = bill * .10
+//     billAmount = bill + tip
+//     console.log('Thank you for your business! Your total is $' + billAmount + '.');
+//     }
+// }
+
+// totalAmount(100, 'fair')
+
+
+
+
+// Tip Calculator 3
+
+// function splitAmount(bill, service, seats){
+//     if(service == 'good'){
+//         tip = bill * .2
+//         totalAmount = bill + tip
+//         splitCheck = totalAmount / seats
+//         console.log('Thank you for your business! The amount owed by each person is $' + splitCheck + '.');
+//     }
+//     else if(service == 'fair'){
+//         tip = bill * .15
+//         totalAmount = bill + tip
+//         splitCheck = totalAmount / seats
+//         console.log('Thank you for your business! The amount owed by each person is $' + splitCheck + '.');
+//     }
+//     else if(service == 'bad'){
+//         tip = bill * .1
+//         totalAmount = bill + tip
+//         splitCheck = totalAmount / seats
+//         console.log('Thank you for your business! The amount owed by each person is $' + splitCheck + '.');
+//     }
+// }
+
+// splitAmount(100, 'good', 2)
