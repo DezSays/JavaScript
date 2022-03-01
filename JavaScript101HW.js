5b6bb781c433a6c5eccaeaf58481d5a8c584a34a
Dez <dezareabryan@gmail.com>
2022-02-28 19:46:43 -0500

    pushing

---
+++ JavaScript101HW.js
@@ -0,0 +1,56 @@
+
+// Madlib
+
+// function madlib(name, subject) {
+//     console.log(name + "'s favorite subject in school is " + subject)
+// }
+
+// madlib("Anushka", "art");
+
+
+
+
+// Tip Calculator 1
+
+// function tipAmount(bill, service){
+//     if(service == 'good'){
+//         tip = bill * .2
+//         console.log(tip);
+//     }
+//     else if(service == 'fair'){
+//         tip = bill * .15
+//         console.log(tip);
+//     }
+//     else if(service == 'bad'){
+//         tip = bill * .10
+//         console.log(tip);
+//     }
+// }
+
+// tipAmount(100, 'fair')
+
+
+
+
+// Tip Calculator 2
+
+// function totalAmount(bill, service){
+//     if(service == 'good'){
+//         tip = bill * .2
+//         billAmount = bill + tip
+//         console.log('Thank you for your business! Your total is $' + billAmount + '.');
+//     }
+//     else if(service == 'fair'){
+//         tip = bill * .15
+//         billAmount = bill + tip
+//         console.log('Thank you for your business! Your total is $' + billAmount + '.');
+//     }
+//     else if(service == 'bad'){
+//     tip = bill * .10
+//     billAmount = bill + tip
+//     console.log('Thank you for your business! Your total is $' + billAmount + '.');
+//     }
+// }
+
+// totalAmount(100, 'fair')
+
